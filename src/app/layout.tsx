58c597d97d960988a8743e2a6a24b3b9fd7081e8
nanamitoshimasu <5nanami1@gmail.com>
2023-09-15 20:28:24 +0900

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import {MkFooter, MkHeader} from "@/app/components";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next.js ミートアップ【鹿児島.mk】 サンプルブログ',
+  description: '2023-09-15 のNext.js ミートアップ【鹿児島.mk】で作成したサンプルブログです。',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <MkHeader />
+      {children}
+        <MkFooter />
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import styles from './page.module.css'
+import {Content, SpearlyApiClient} from "@spearly/sdk-js";
+import Link from "next/link";
+
+const apiClient = new SpearlyApiClient(process.env.API_KEY as string)
+
+export default async function Home() {
+  const contents = await apiClient.getList("sample-post")
+
+  return (
+    <main className={styles.main}>
+      <article>
+        <ul>
+          {
+            (contents.data).map((content: Content) => {
+              const values = content.values as any;
+              const path = `/blog/${content.attributes.publicUid}`
+
+              return (
+                <li className={styles.articleList} key={content.id}>
+                  <Link href={path}>
+                      {values.title}
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </article>
+    </main>
+  )
+}
